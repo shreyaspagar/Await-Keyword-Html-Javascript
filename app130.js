613f05afc8b3c1a1ac173f506bb5dd6dfbcfe9ea
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-06-12 11:52:40 +0530

    Add files via upload

---
+++ app130.js
@@ -0,0 +1,52 @@
+// function getNo() {
+//    return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let num  = Math.floor(Math.random() * 10) + 1;
+//          console.log(num);
+//         resolve();
+//     }, 1000);
+    
+//    });
+// }
+
+// async function demo() {
+//    await getNo();
+//    await getNo();
+//    await getNo();
+//    await getNo();
+//     getNo();
+// 
+
+h1 = document.querySelector("h1");
+
+function changeColor(color,delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let num  = Math.floor(Math.random() * 5) + 1;
+            if (num > 3) {
+                reject("promise rejected");
+            }
+
+             h1.style.color = color; 
+             console.log(`color changed to ${color}!`);
+             resolve("color changed!");   
+    }, delay);
+});
+}   
+
+async function demo() {
+    try {
+
+    
+    await changeColor("red", 1000);
+   await changeColor("orange", 1000);
+    await changeColor("green", 1000);
+   await changeColor("blue", 1000);
+    } catch(err) {
+        console.log("error caught");
+        console.log(err);
+    }
+   let a = 5;
+   console.log(a);
+   console.log("new number = ", a + 3);
+}
